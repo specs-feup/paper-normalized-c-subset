7b3de98abc9f14b68c87169ad79f272eba9c9385
João Bispo <joaobispo@gmail.com>
2022-10-25 01:25:18 +0100

    Adds script for processing execution times

--- ExtractCsv.js
+++ ExtractCsv.js
@@ -4,12 +4,29 @@ laraImport("lara.Io");
 
 const conv = new JsonConverter();
 
-// Load inlining results
-const inliningData = Io.readJson(
-  "results/inliner-test (All 2022-10-23 Linux).json"
-);
+function inliningResults() {
+  // Load inlining results
+  const inliningData = Io.readJson(
+    "results/inliner-test (All 2022-10-23 Linux).json"
+  );
 
-const inlinedCsv = conv.inliningResults(inliningData);
-Io.writeFile("inlinedCalls.csv", inlinedCsv);
-println("Wrote 'inlinedCalls.csv'");
-println(inlinedCsv);
+  const inlinedCsv = conv.inliningResults(inliningData);
+  Io.writeFile("inlinedCalls.csv", inlinedCsv);
+  println("Wrote 'inlinedCalls.csv'");
+  println(inlinedCsv);
+}
+
+function executionResults() {
+  // Load execution results
+  const executionData = Io.readJson(
+    "results/inliner-execution-test (All 2022-10-24 Linux).json"
+  );
+
+  const executionCsv = conv.executionResults(executionData);
+
+  Io.writeFile("executionComparison.csv", executionCsv);
+  println("Wrote 'executionComparison.csv'");
+  println(executionCsv);
+}
+
+executionResults();
--- src-lara/JsonConverter.js
+++ src-lara/JsonConverter.js
@@ -1,3 +1,5 @@
+laraImport("lara.Strings");
+
 class JsonConverter {
   inliningResultsV1(
     obj,
@@ -132,5 +134,147 @@ class JsonConverter {
     }
 
     return benchmarkSetsNames;
+  }
+
+  executionResults(obj) {
+    // Examples:
+    // Inlining_-O0_gcc_2
+    // NotInlining_-O2_gcc
+
+    const configs = [
+      ["NotInlining", "Inlining"],
+      ["-O0", "-O2"],
+      ["clang", "gcc"],
+    ];
+
+    // Find first set that uses inlining
+    let baseSet = undefined;
+    for (const key in obj) {
+      if (key.startsWith("Inlining_")) {
+        baseSet = obj[key];
+        break;
+      }
+    }
+
+    if (baseSet === undefined) {
+      throw new Error("Could not find base set");
+    }
+
+    // Get list of benchmark set-instance pair
+    let benchmarkSetsNames = this.#getBenchmarkSetsNames(obj);
+
+    // Filter benchmarks that were successful
+    benchmarkSetsNames = benchmarkSetsNames.filter((names) =>
+      this.#isSuccessExecutionSet(names, baseSet)
+    );
+
+    // Extract runs
+    const runs =
+      baseSet[benchmarkSetsNames[0][0]][benchmarkSetsNames[0][1]]["result"][
+        "runs"
+      ];
+
+    const data = [];
+
+    const header = ["Benchmark"];
+    for (const comp of configs[2]) {
+      for (const opt of configs[1]) {
+        const base = comp + "_" + opt + " Base (ms)";
+        header.push(base);
+        header.push("Speedup");
+      }
+    }
+
+    data.push(header);
+
+    // For each benchmark, extract data using the sequence of configurations
+    for (const benchmarkNames of benchmarkSetsNames) {
+      const row = [];
+      row.push(benchmarkNames[1]);
+      let counter = 0;
+      let baseTime = undefined;
+      for (const comp of configs[2]) {
+        for (const opt of configs[1]) {
+          for (const inlining of configs[0]) {
+            const setName = inlining + "_" + opt + "_" + comp + "_" + runs;
+            const set = obj[setName];
+
+            if (set === undefined) {
+              throw new Error("Could not find set " + setName);
+            }
+
+            const data =
+              set[benchmarkNames[0]][benchmarkNames[1]]["result"]["runOutputs"];
+
+            const execTimes = data.map(this.#extractExecutionTime);
+
+            if (counter % 2 == 0) {
+              const sum = execTimes.reduce((a, b) => a + b, 0);
+              const avg = sum / execTimes.length;
+              baseTime = avg;
+              row.push(baseTime);
+            } else {
+              const sum = execTimes.reduce((a, b) => a + b, 0);
+              const avg = sum / execTimes.length;
+              const speedup = baseTime / avg;
+              row.push(speedup);
+            }
+            counter++;
+            //println("Exec times: " + execTimesRaw);
+
+            // Get execution times
+            //printlnObject(data);
+            //row.push();
+          }
+        }
+      }
+
+      data.push(row);
+    }
+
+    return this.toCsv(data);
+  }
+
+  #extractExecutionTime(output) {
+    const prefix = "Test execution time: ";
+    // Extract correct line
+    let execTime = Strings.extractValue(prefix, output);
+    /*
+    println("Exec time raw: " + execTimeRaw);
+    println("Exec time raw len: " + execTimeRaw.length);
+    println("Prefix: " + prefix);
+    println("Prefix len: " + prefix.length);
+    */
+    //let execTime = execTimeRaw.substring(prefix.length, execTimeRaw.length);
+
+    if (!execTime.endsWith("ms")) {
+      throw new Error("Expected to end with 'ms': '" + execTime + "'");
+    }
+
+    execTime = execTime.substring(0, execTime.length - "ms".length);
+
+    return Number(execTime);
+  }
+
+  #isSuccessExecutionSet(names, obj) {
+    const data = obj[names[0]][names[1]];
+
+    // Check success
+    if (!data["success"]) {
+      println(`Ignoring ${names[1]}`);
+      return false;
+    }
+
+    // Check return values
+    const returnValues = data["result"]["returnValues"];
+
+    for (const value of returnValues) {
+      if (value !== 0) {
+        println(`Ignoring ${names[1]}`);
+        return false;
+      }
+    }
+
+    return true;
   }
 }
